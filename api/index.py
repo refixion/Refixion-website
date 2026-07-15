4096dc663505c1c8aa861df57a63087d5b506667
refixion <refixionstore@gmail.com>
2026-07-15 21:48:23 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 import sys
 import os
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "../backend"))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../backend")))
 
 from server import app
